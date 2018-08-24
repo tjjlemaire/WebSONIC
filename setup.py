f52e8e5370135dbd3de4628c817b59beabd5ecdc
Theo Lemaire <theo.lemaire@epfl.ch>
2018-08-24 18:20:35 +0200

    updated setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # @Date:   2017-06-13 09:40:02
 # @Email: theo.lemaire@epfl.ch
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2018-08-24 17:15:57
+# @Last Modified time: 2018-08-24 18:20:04
 
 from setuptools import setup
 
@@ -14,35 +14,37 @@ def readme():
         return f.read()
 
 
-setup(name='WebSONIC',
-      version='1.0',
-      description='Interactive web application to view the predicted electrical response of \
-                   different neuron types to ultrasonic stimuli for various combinations of \
-                   sonication parameters, computed with the **SONIC** model. A comparative module \
-                   to explore predicted responses of the same neurons to electrical stimuli \
-                   (computed with standard Hodgkin-Huxley equations) is also included.',
-      long_description=readme(),
-      url='???',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Science/Research',
-          'Programming Language :: Python :: 3',
-          'Topic :: Scientific/Engineering :: Physics'
-      ],
-      keywords=('SONIC NICE acoustic ultrasound ultrasonic neuromodulation neurostimulation excitation\
-                 computational model intramembrane cavitation'),
-      author='Théo Lemaire',
-      author_email='theo.lemaire@epfl.ch',
-      license='MIT',
-      packages=['WebSONIC'],
-      scripts=['run.py'],
-      install_requires=[
-          'dash==0.24.0',
-          'dash-renderer==0.13.0',
-          'dash-html-components==0.11.0'
-          'dash-core-components==0.27.1',
-          'dash-auth==1.0.0',
-          'pysftp==0.2.9',
-          'colorlover==0.2.1',
-      ],
-      zip_safe=False)
+setup(
+    name='WebSONIC',
+    version='1.0',
+    description='Interactive web application to view the predicted electrical response of \
+               different neuron types to ultrasonic stimuli for various combinations of \
+               sonication parameters, computed with the **SONIC** model. A comparative module \
+               to explore predicted responses of the same neurons to electrical stimuli \
+               (computed with standard Hodgkin-Huxley equations) is also included.',
+    long_description=readme(),
+    url='???',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    keywords=('SONIC NICE acoustic ultrasound ultrasonic neuromodulation neurostimulation excitation\
+              computational model intramembrane cavitation'),
+    author='Théo Lemaire',
+    author_email='theo.lemaire@epfl.ch',
+    license='MIT',
+    packages=['WebSONIC'],
+    scripts=['run.py'],
+    install_requires=[
+        'dash==0.24.0',
+        'dash-renderer==0.13.0',
+        'dash-html-components==0.11.0'
+        'dash-core-components==0.27.1',
+        'dash-auth==1.0.0',
+        'pysftp==0.2.9',
+        'colorlover==0.2.1'
+    ],
+    zip_safe=False
+)
